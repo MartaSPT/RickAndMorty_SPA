9678291c2856aa289513fb1e52736c06465bf7d1
Marta Trincão <marta.spt@hotmail.com>
2024-02-20 08:54:00 +0100

    feature id

--- spa-skeleton/app/controller/singlepage-c.js
+++ spa-skeleton/app/controller/singlepage-c.js
@@ -4,7 +4,8 @@ import singlePageView from '../view/singlepage-v.js';
 
 export default async function init() {
     let url = (window.location).toString();
-    let id = parseInt(url.charAt(url.length-1));
+    url = url.split("/");
+    let id = parseInt(url[url.length-1]);
     
     let character = await getCharacter(id);
     singlePageView(character);
